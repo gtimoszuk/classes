d0425f4e9dbaaf0f78699dc3fdef679db89309c3
Grzegorz Timoszuk <gtimoszuk@mimuw.edu.pl>
2011-03-13 22:23:55 +0100

    simple test

---
+++ db4o-examples/src/main/java/pl/edu/mimuw/gtimoszuk/db4o/domain/Pilot.java
@@ -0,0 +1,27 @@
+package pl.edu.mimuw.gtimoszuk.db4o.domain;
+
+public class Pilot {
+	private String name;
+	private int points;
+
+	public Pilot(String name, int points) {
+		this.name = name;
+		this.points = points;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void addPoints(int points) {
+		this.points += points;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String toString() {
+		return name + "/" + points;
+	}
+}
---
+++ db4o-examples/src/test/java/pl/edu/mimuw/gtimoszuk/db4o/EmptyDB4OCreator.java
@@ -0,0 +1,46 @@
+package pl.edu.mimuw.gtimoszuk.db4o;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import pl.edu.mimuw.gtimoszuk.db4o.domain.Pilot;
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+
+/**
+ * This class only purpose is to create a db to be presented in OME, so no
+ * deletes at all
+ * 
+ * @author gtimoszuk
+ * 
+ */
+public class EmptyDB4OCreator {
+
+	private static final String DB4OFILENAME = "/home/ballo0/GTI/zajecia/db4o/sampleDB";
+
+	ObjectContainer db = null;
+
+	@Before
+	public void dbOpen() {
+		db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(),
+				DB4OFILENAME);
+	}
+
+	@Test
+	public void piotCreator() {
+
+		try {
+			Pilot pilot1 = new Pilot("Michael Schumacher", 100);
+			db.store(pilot1);
+			System.out.println("Stored " + pilot1);
+			Pilot pilot2 = new Pilot("Rubens Barrichello", 99);
+			db.store(pilot2);
+			System.out.println("Stored " + pilot2);
+		} finally {
+			db.close();
+		}
+
+	}
+
+}
