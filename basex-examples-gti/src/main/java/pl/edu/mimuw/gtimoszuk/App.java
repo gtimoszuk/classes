798c259cf731abb744a5019ac7caf4b048f5b033
Grzegorz Timoszuk <gtimoszuk@mimuw.edu.pl>
2011-05-08 14:47:20 +0200

    basex examples project

---
+++ basex-examples-gti/src/main/java/pl/edu/mimuw/gtimoszuk/App.java
@@ -0,0 +1,13 @@
+package pl.edu.mimuw.gtimoszuk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
